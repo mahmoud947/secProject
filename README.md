edacc7632fbdd6c2ac9f71b0550a3776e882598e
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-03-31 13:59:00 +0200

    deploy

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# testttttttt2py
-in section
+#Validation system
+
+Team Members:
+Omar Nabil 2018030089 (Team Leader)
+Heidi Mohamed Samir 2018030168
+Youstina Nashaat 2018030269
+Abdelrahman Fawzy 2018030068
+Mohamed Hassan 2018030119
+Amr Ahmed 2018030092
+Ahmed Esmail 2018030183
+Mohamed Ali 2018030131
+Mostafa Ashraf 2018030154
+Mahmoud Kamal 2018030148
+
+1. Ask the user if he wants to validate his email or phone number first.
+   2.If he chose to enter email: check if the email is valid or not so if the email is valid print the email is valid. but if the email is invalid display error message.
+2. But if he enters his phone number, first should check the length of the number should be (11 number).
+   So, if the number is correct display the network type and accept it.
+   But if it isn’t valid display error data.
---
+++ task33.py
@@ -0,0 +1,62 @@
+from operator import truediv
+
+
+def validate_email(emaill):
+    valuu = emaill.split("@")
+    host = str(valuu[1])
+    host2 = host.split(".")
+    return host2[0]
+
+
+def get_host(host):
+    if host == "gmail":
+        return "it is gmail"
+    elif host == "yahoo":
+        return "it is yahoo"
+    elif host == "hotmail":
+        return "it is hotmail"
+    else:
+        return "Error"
+
+
+def check_len(num):
+    if len(num) == 11:
+        return True
+    else:
+        return False
+
+
+def validate_phone(num):
+    com = num[0:3]
+    if check_len(num):
+        if com == "010":
+            return "vodafone"
+        elif com == "011":
+            return "Etisalate"
+        elif com == "012":
+            return "Orange"
+        elif com == "015":
+            return "We"
+        else:
+            return "Error"
+    else:
+        return "the number should be 11 digte nume"
+
+
+def select_validation_type():
+    print("select validation type\n")
+    ine = input("1- phone \n2- email \n0-exit\n ")
+    inee = int(ine)
+    if inee == 1:
+        phone = input("enter your phone\n")
+        print(validate_phone(phone))
+        select_validation_type()
+    elif inee == 2:
+        emails = input("enter your email\n")
+        print(validate_email(emails))
+        select_validation_type()
+    else:
+        return
+
+
+select_validation_type()
